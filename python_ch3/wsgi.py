575ae3ed3c7d0d790c8bfafa5e747b861be76bcb
nelldkLee <nelldklee@gmail.com>
2019-07-02 16:36:05 +0900

    리눅스 시스템에 해당 프로젝트 경로 추가를 위한 설정 추가

--- python_ch3/wsgi.py
+++ python_ch3/wsgi.py
@@ -9,6 +9,10 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_ch3.settings')
